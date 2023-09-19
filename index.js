a87bf5923cd7af03ffc71dd30664f9ea0afefe01
aa-r-ju <aarju3447@gmail.com>
2023-09-19 16:53:02 +0545

    feat:completed part3 video d

--- index.js
+++ index.js
@@ -3,6 +3,33 @@ console.log("Hello There")
 const express = require('express');
 const { request } = require('http');
 let app = express();
+const mongoose = require('mongoose')
+
+const url =`mongodb+srv://aarju_me:aarju_me123@cluster-test.kzjo2vb.mongodb.net/?retryWrites=true&w=majority`
+
+
+mongoose.set('strictQuery',false)
+mongoose.connect(url)
+
+const noteSchema = new mongoose.Schema({
+  content: String,
+  important: Boolean,
+})
+
+noteSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
+
+const Note = mongoose.model('Note', noteSchema)
+
+
+
+
+
 const cors = require('cors')
 app.use(express.json())
 app.use(express.static("build"))
@@ -17,31 +44,36 @@ const requestLogger = (request, response, next) => {
   }
   app.use(requestLogger)
 
+const notes = []
 
-let notes = [
-    {
-      id: 1,
-      content: "HTML is easy",
-      important: true
-    },
-    {
-      id: 2,
-      content: "Browser can execute only JavaScript",
-      important: false
-    },
-    {
-      id: 3,
-      content: "GET and POST are the most important methods of HTTP protocol",
-      important: true
-    }
-  ]
+
+// let notes = [
+//     {
+//       id: 1,
+//       content: "HTML is easy",
+//       important: true
+//     },
+//     {
+//       id: 2,
+//       content: "Browser can execute only JavaScript",
+//       important: false
+//     },
+//     {
+//       id: 3,
+//       content: "GET and POST are the most important methods of HTTP protocol",
+//       important: true
+//     }
+//   ]
   
   app.get("/",(request, response) => {
     response.send("<h1>Hello World</h1>")
   })
 
   app.get("/api/notes",(request, response) => {
-  response.json(notes)
+    Note.find({}).then((result)=> {
+      response.json(result)
+
+    })
 })
 
 app.get("/api/notes/:id",(request, response) => {
@@ -73,6 +105,16 @@ app.get("/api/notes/:id",(request, response) => {
     myNewPost.id = notes.length + 1
     notes.push(myNewPost)
     response.status(201).json(myNewPost)
+
+    // const note = new Note({
+    //   content: 'HTML is Easy',
+    //   important: true,     
+    // })
+    
+    // note.save().then(result => {
+    //   console.log('note saved!')
+    //   mongoose.connection.close()
+    // })
 });
   
 app.use((request, response, next) => {
