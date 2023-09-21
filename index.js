fc3f26047e49e94be3f82d496b3bb945b8c1e227
aa-r-ju <aarju3447@gmail.com>
2023-09-21 22:48:37 +0545

    feat:completed part3 c video 5

--- index.js
+++ index.js
@@ -76,50 +76,80 @@ const notes = []
     })
 })
 
-app.get("/api/notes/:id",(request, response) => {
-    const myId = Number(request.params.id)
-    const myNote = notes.find((note) => note.id === myId)
+app.get("/api/notes/:id",(request, response,next) => {
+  Note.findById(request.params.id).then((result) => {
+    if (result) {
+      response.json(result)
+  } else {
+      response.status(404).send(`There are no notes at ${request.params.id}`)
+  }
+  }).catch(e => {
+    next(e)
+    // console.log(e)
+    // response.status(500).send(`${request.params.id}is not in the required format`)
+  })
 
-    if (myNote) {
-        response.json(myNote)
-    } else {
-        response.status(404).send(`There are no notes at ${myId}`)
+    // const myId = Number(request.params.id)
+    // const myNote = notes.find((note) => note.id === myId)
+
+   
+  })
+
+  app.put('/api/notes/:id', (request, response, next) => {
+    const body = request.body
+  
+    const note = {
+      content: body.content,
+      important: body.important,
     }
+  
+    Note.findByIdAndUpdate(request.params.id, note, { new: true })
+      .then(updatedNote => {
+        response.json(updatedNote)
+      })
+      .catch(error => next(error))
   })
 
-  app.put('/api/notes/:id', (request, response) => {
-    const resourceId = Number(request.params.id);
-    const updatedData = request.body;
-   notes =  notes.map(note => note.id !== resourceId ? note : updatedData)
-    response.json(updatedData);
-  });
-
-  app.delete("/api/notes/:id",(request, response) => {
-    const myId = Number(request.params.id)
-     notes = notes.filter((note) => note.id !== myId)
-
-    response.status(204).send(`The notes at ${myId} has been deleted`)
+  app.delete('/api/notes/:id', (request, response, next) => {
+    Note.findByIdAndRemove(request.params.id)
+      .then(result => {
+        response.status(204).end()
+      })
+      .catch(error => next(error))
   })
-  app.post("/api/notes",(request,response) => {
-    const myNewPost = request.body;
-    myNewPost.id = notes.length + 1
-    notes.push(myNewPost)
-    response.status(201).json(myNewPost)
-
-    // const note = new Note({
-    //   content: 'HTML is Easy',
-    //   important: true,     
-    // })
-    
-    // note.save().then(result => {
-    //   console.log('note saved!')
-    //   mongoose.connection.close()
-    // })
-});
+  app.post('/api/notes', (request, response) => {
+    const body = request.body
+  
+    if (body.content === undefined) {
+      return response.status(400).json({ error: 'content missing' })
+    }
+  
+    const note = new Note({
+      content: body.content,
+      important: body.important || false,
+    })
+  
+    note.save().then(savedNote => {
+      response.json(savedNote)
+    })
+  })
   
 app.use((request, response, next) => {
     response.status(404).send("no code available to handle this request")
   })
+
+  const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+  
+    if (error.name === 'CastError') {
+      return response.status(400).send({ error: 'malformatted id' })
+    } 
+  
+    next(error)
+  }
+  
+  // this has to be the last loaded middleware.
+  app.use(errorHandler)
 
 const PORT = process.env.PORT ? process.env.PORT: 3001;
 app.listen(PORT);
